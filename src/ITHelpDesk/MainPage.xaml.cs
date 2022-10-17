1211e0042ba9a7d390d5a6ace172c262ca69c3f8
MOROZIUK Yurii <Yurii.Moroziuk@gunvorgroup.com>
2022-10-17 16:26:39 +0300

    add config with phone number

--- src/ITHelpDesk/MainPage.xaml.cs
+++ src/ITHelpDesk/MainPage.xaml.cs
@@ -1,23 +1,29 @@
-﻿namespace ITHelpDesk;
+﻿using ITHelpDesk.Configuration;
+
+using Microsoft.Extensions.Configuration;
+
+namespace ITHelpDesk;
 
 public partial class MainPage : ContentPage
 {
-	public MainPage()
+	public MainPage(IConfiguration configuration)
 	{
 		InitializeComponent();
+
+		var itSupportPhone = configuration.GetRequiredSection(nameof(ItSupportPhone))
+			.Get<ItSupportPhone>();
+
+		NumberEntry.Text = itSupportPhone.PhoneNumber;
 	}
 
 	private void RadButton_Clicked(object sender, EventArgs e)
 	{
-		//https://docs.telerik.com/devtools/maui/get-started/install-nuget?_ga=2.30423935.1770633938.1665677395-1774374380.1553538507&_gl=1*w06kmc*_ga*MTc3NDM3NDM4MC4xNTUzNTM4NTA3*_ga_9JSNBCSF54*MTY2NTc2MzQyNy4yLjEuMTY2NTc2NDQwMC4wLjAuMA..
 		var number = NumberEntry.Text;
 
 		if (PhoneDialer.Default.IsSupported)
 		{
 			PhoneDialer.Default.Open(number);
 		}
-
-		SemanticScreenReader.Announce("yo");
 	}
 }
 
--- src/ITHelpDesk/MauiProgram.cs
+++ src/ITHelpDesk/MauiProgram.cs
@@ -1,4 +1,6 @@
-﻿using Telerik.Maui.Controls.Compatibility;
+﻿using System.Reflection;
+using Microsoft.Extensions.Configuration;
+using Telerik.Maui.Controls.Compatibility;
 
 namespace ITHelpDesk;
 
@@ -16,6 +18,23 @@ public static class MauiProgram
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
 			});
 
+		RegisterConfig(builder);
+
+		builder.Services.AddTransient<MainPage>();
+
 		return builder.Build();
+	}
+
+	private static void RegisterConfig(MauiAppBuilder builder)
+	{
+		var a = Assembly.GetExecutingAssembly();
+		using var stream = a.GetManifestResourceStream("ITHelpDesk.appsettings.json");
+
+		var config = new ConfigurationBuilder()
+			.AddJsonStream(stream)
+			.Build();
+
+
+		builder.Configuration.AddConfiguration(config);
 	}
 }
